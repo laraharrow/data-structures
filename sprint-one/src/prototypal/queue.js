c334734974945b4493accc052f86d30719d94170
Hack Reactor Student <student@hackreactor.com>
2017-08-23 16:31:06 -0700

    Implement prototypal queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,44 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  //var instanceObj = Object.create(Queue.prototype);
+  var instanceObj = Object.create(queueMethods);
+  instanceObj.storage = {};
+  instanceObj.count = 0;
+  instanceObj.first = 0;
+  return instanceObj;
 };
 
-var queueMethods = {};
-
-
+// Queue.prototype.enqueue = function(value) {
+//   this.storage[this.count + this.first] = value;
+//   this.count++;
+// };
+// Queue.prototype.dequeue = function() {
+//   if (this.count > 0) {
+//     var result = this.storage[`${this.first}`];
+//     delete this.storage[`${this.first}`];
+//     this.first++;
+//     this.count--;
+//     return result;
+//   }  
+// };
+// Queue.prototype.size = function() {
+//   return this.count;
+// };
+// var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    this.storage[this.count + this.first] = value;
+    this.count++;
+  },
+  dequeue: function() {
+    if (this.count > 0) {
+      var result = this.storage[`${this.first}`];
+      delete this.storage[`${this.first}`];
+      this.first++;
+      this.count--;
+      return result;
+    }  
+  },
+  size: function() {
+    return this.count;
+  }
+};
