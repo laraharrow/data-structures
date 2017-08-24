dca621c47e2b786c6dd1a1724c886be9b50a0026
Hack Reactor Student <student@hackreactor.com>
2017-08-24 15:38:57 -0700

    Implement skeleton for binary search tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,22 @@
 var BinarySearchTree = function(value) {
+  var bst = Object.create(BinarySearchTree.prototype);
+  bst.value = value;
+  bst.right = undefined;
+  bst.left = undefined;
+  return bst;
 };
 
+BinarySearchTree.prototype.insert = function(value) {
+  
+};
+
+BinarySearchTree.prototype.contains = function(value) {
+  
+};
+
+BinarySearchTree.prototype.depthFirstLog = function(cb) {
+  
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
